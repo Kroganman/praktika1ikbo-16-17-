e456743cd17e49e8ab4d0de4586b3d7a8e947f82
Kroganman <31574752+Kroganman@users.noreply.github.com>
2018-09-23 21:51:49 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+package prac1;
+
+class Main
+{
+	public static void main (String[] args)
+	{
+		Ball ball1 = new Ball("Yellow and blue ","volleyball ", 10 );
+		System.out.println(ball1.toString());
+		Book book1 = new Book("War and peace ", "L.N.Tolstoy ",1270);
+		System.out.println(book1.toString());
+	}
+}
